7c540c2723d1352cdd9aff0c02f54cda15b74cf5
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-05-07 20:12:34 -0500

    Add version numbering to package

---
+++ planingfsi/__init__.py
@@ -0,0 +1,8 @@
+"""PlaningFSI is a Python package for solving 2d FSI problems.
+
+The fluid solver is based on linearized potential-flow assumptions,
+and the structural solver considers a large-deformation simple beam element.
+
+"""
+VERSION = (1, 0, 'dev')
+__version__ = '.'.join(map(str, VERSION))
