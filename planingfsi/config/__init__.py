bc8daa3ae369226071cc2197852bfe15fda401c6
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-05-07 21:51:09 -0500

    Code cleanup

--- planingfsi/config/__init__.py
+++ planingfsi/config/__init__.py
@@ -1,12 +1,13 @@
-from .config import (flow,
-                     body,
-                     path,
-                     plotting,
-                     io,
-                     solver,
-                     ramp,
-                     has_free_structure,
-                     it_dir,
-                     it
-                     )
+from .config import (
+    flow,
+    body,
+    path,
+    plotting,
+    io,
+    solver,
+    ramp,
+    has_free_structure,
+    it_dir,
+    it
+)
 del config
