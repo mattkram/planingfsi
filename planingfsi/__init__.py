3a45bcdd1d233020208c0c270c0329c1702b2e30
Matt Kramer <mkramer@chevron.com>
2018-08-01 11:42:55 -0500

    Update mesh CLI.

--- planingfsi/__init__.py
+++ planingfsi/__init__.py
@@ -1,8 +1,17 @@
 """PlaningFSI is a Python package for solving 2d FSI problems.
 
+PlaningFSI solves the general FSI problem of 2-D planing surfaces on a free
+surface, where each surface can be rigid or flexible. The substructures are
+part of a rigid body, which can be free in sinkage and trim.
+
 The fluid solver is based on linearized potential-flow assumptions,
 and the structural solver considers a large-deformation simple beam element.
 
 """
+import logging
+
 VERSION = (1, 0, 'dev')
 __version__ = '.'.join(map(str, VERSION))
+
+logger = logging.getLogger('planingfsi')
+logger.setLevel(logging.DEBUG)
