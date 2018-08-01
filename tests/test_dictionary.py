83d4a9aa0d96e6ef4da8112bafb08ab9fe61ef34
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-08-01 12:54:38 -0500

    Migrate tests to pytest.

--- tests/test_dictionary.py
+++ tests/test_dictionary.py
@@ -1,67 +1,60 @@
 import math
-import os.path
-import unittest
 
-import context
-
-import planingfsi.io
-from planingfsi import io
+from planingfsi.io import Dictionary
 
 
-DEBUG = False
+def assert_equal_instance(dict_, key, target_val, type_):
+    """Assert the dictionary value equals the target and is of the expected type."""
+    dict_val = dict_.read(key)
+    assert isinstance(dict_val, type_)
+    assert target_val == dict_val
 
 
-class DictionaryTest(unittest.TestCase):
+def assert_dict(dict_, type_):
+    """Assert that all instances in a dict have values of the required type."""
+    for key, val in dict_.items():
+        assert_equal_instance(dict_, key, val, type_)
 
-    @classmethod
-    def setUpClass(cls):
-        cls.dict_ = planingfsi.io.Dictionary(os.path.join(context.TEST_DIR, 'testDict'))
-        if DEBUG:
-            for key, val in cls.dict_.items():
-                print('{0}: {1}'.format(key, val), type(val))
-                if isinstance(val, dict):
-                    for keyj, valj in val.items():
-                        print('{0} = {1}'.format(keyj, valj), type(valj))
 
-    def assertEqualInstance(self, key, target_val, type_):
-        dict_val = self.dict_.read(key)
-        self.assertIsInstance(dict_val, type_)
-        self.assertEqual(target_val, dict_val)
+def test_read_bool():
+    dict_ = Dictionary({
+        'boolTrueUpper': True,
+        'boolTrueLower': True,
+        'boolFalseUpper': False,
+        'boolFalseLower': False
+    })
+    assert_dict(dict_, bool)
 
-    def assertDict(self, dict_, type_):
-        for key, val in dict_.items():
-            with self.subTest(key=key):
-                self.assertEqualInstance(key, val, type_)
 
-    def test_read_bool(self):
-        dict_ = {'boolTrueUpper': True,
-                 'boolTrueLower': True,
-                 'boolFalseUpper': False,
-                 'boolFalseLower': False}
-        self.assertDict(dict_, bool)
+def test_read_int():
+    dict_ = Dictionary({
+        'intMultipleDigits': 2999,
+        'intSingleDigit': 2
+    })
+    assert_dict(dict_, int)
 
-    def test_read_int(self):
-        dict_ = {'intMultipleDigits': 2999,
-                 'intSingleDigit': 2}
-        self.assertDict(dict_, int)
 
-    def test_read_nan(self):
-        for key in ['nanUnsigned', 'nanNegative', 'nanPositive']:
-            val = self.dict_.read(key)
-            self.assertIsInstance(val, float)
-            self.assertTrue(math.isnan(val))
+def test_read_nan(test_dict):
+    for key in ['nanUnsigned', 'nanNegative', 'nanPositive']:
+        val = test_dict.read(key)
+        assert isinstance(val, float)
+        assert math.isnan(val)
 
-    def test_read_infs(self):
-        INF = float('inf')
-        dict_ = {'infUnsigned': INF,
-                 'infPositive': INF,
-                 'infNegative': -INF}
-        self.assertDict(dict_, float)
 
-    def test_read_none(self):
-        for upper_lower in ['Upper', 'Lower']:
-            self.assertIsNone(self.dict_.read('none{0}'.format(upper_lower)))
+def test_read_infs():
+    infinity = float('inf')
+    dict_ = Dictionary({
+        'infUnsigned': infinity,
+        'infPositive': infinity,
+        'infNegative': -infinity
+    })
+    assert_dict(dict_, float)
 
-if __name__ == '__main__':
-    unittest.main()
 
+def test_read_none(test_dict):
+    for upper_lower in ['Upper', 'Lower']:
+        assert test_dict.read('none{0}'.format(upper_lower)) is None
+
+
+def test_read_env_var(test_dict):
+    assert test_dict['envVar'] == 'Dummy'
