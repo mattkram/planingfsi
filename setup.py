728605ef2041bffccd5f2e9a1231305a2103a397
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-08-05 08:21:26 -0500

    Add pytest to setup script.

--- setup.py
+++ setup.py
@@ -8,17 +8,35 @@ def readme():
         return f.read()
 
 
-setup(name='planingfsi',
-      version=planingfsi.__version__,
-      description='FSI for large-deformation planing surfaces',
-      long_description=readme(),
-      author='Matthew Kramer',
-      author_email='matthew.robert.kramer@gmail.com',
-      license='MIT',
-      packages=['planingfsi'],
-      entry_points={
-          'console_scripts': [
-              'planingFSI=planingfsi.command_line.planingfsi:main',
-              'generateMesh=planingfsi.command_line.mesh:main'
-          ]
-      })
+requirements = [
+    'matplotlib',
+    'numpy',
+    'scipy',
+    'six',
+    'click',
+    'pytest-runner'
+]
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'coverage',
+]
+
+setup(
+    name='planingfsi',
+    version=planingfsi.__version__,
+    description='FSI for large-deformation planing surfaces',
+    long_description=readme(),
+    author='Matthew Kramer',
+    author_email='matthew.robert.kramer@gmail.com',
+    license='MIT',
+    packages=['planingfsi'],
+    setup_requires=requirements,
+    tests_require=test_requirements,
+    entry_points={
+        'console_scripts': [
+            'planingFSI=planingfsi.command_line.planingfsi:main',
+            'generateMesh=planingfsi.command_line.mesh:main'
+        ]
+    }
+)
