f1fe5ad73a95c26476fee3769e5b13f63615bc93
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-12-27 20:06:16 -0600

    Update for PyPIˆ

--- setup.py
+++ setup.py
@@ -1,43 +1,121 @@
-from setuptools import setup
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import codecs
+import os
+import sys
 
-import planingfsi
+from setuptools import find_packages, setup, Command
 
+here = os.path.abspath(os.path.dirname(__file__))
 
-with open('README.md') as fh:
-    long_description = fh.read()
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
+about = {}
+
+with open(os.path.join(here, "planingfsi", "__version__.py")) as f:
+    exec(f.read(), about)
 
 requirements = [
+    'click',
     'matplotlib',
     'numpy',
     'scipy',
     'six',
-    'click',
-    'pytest-runner'
 ]
-test_requirements = [
-    'pytest',
-    'pytest-cov',
-    'coverage',
-]
+
+# https://pypi.python.org/pypi/stdeb/0.8.5#quickstart-2-just-tell-me-the-fastest-way-to-make-a-deb
+class DebCommand(Command):
+    """Support for setup.py deb"""
+
+    description = "Build and publish the .deb package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "deb_dist"))
+        except FileNotFoundError:
+            pass
+        self.status(u"Creating debian mainfest…")
+        os.system(
+            "python setup.py --command-packages=stdeb.command sdist_dsc -z artful --package3=pipenv --depends3=python3-virtualenv-clone"
+        )
+        self.status(u"Building .deb…")
+        os.chdir("deb_dist/pipenv-{0}".format(about["__version__"]))
+        os.system("dpkg-buildpackage -rfakeroot -uc -us")
+
+
+class UploadCommand(Command):
+    """Support setup.py publish."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except FileNotFoundError:
+            pass
+        self.status("Building Source distribution…")
+        os.system("{0} setup.py sdist bdist_wheel".format(sys.executable))
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+        sys.exit()
+
 
 setup(
     name='planingfsi',
-    version=planingfsi.__version__,
-    description='FSI for large-deformation planing surfaces',
+    version=about["__version__"],
+    description='Fluid-Structure Interaction for large-deformation planing surfaces',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/mattkram/planingfsi",
     author='Matthew Kramer',
     author_email='matthew.robert.kramer@gmail.com',
     license='MIT',
-    packages=['planingfsi'],
+    packages=find_packages(exclude=["bin", "doc", "tests", "tutorials"]),
     setup_requires=requirements,
-    tests_require=test_requirements,
     entry_points={
         'console_scripts': [
             'planingFSI=planingfsi.command_line.planingfsi:main',
             'generateMesh=planingfsi.command_line.mesh:main'
         ]
-    }
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    cmdclass={"upload": UploadCommand, "deb": DebCommand},
 )
