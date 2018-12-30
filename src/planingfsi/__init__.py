8b8dc5a9c2351cb1f721b2bae75904423199ee23
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-12-30 14:40:23 -0600

    Clean up docstrings and load version from pyproject.toml

--- src/planingfsi/__init__.py
+++ src/planingfsi/__init__.py
@@ -1,8 +1,9 @@
-"""PlaningFSI is a Python package for solving 2d FSI problems.
+"""PlaningFSI is a Python package for solving 2-D FSI problems.
 
 PlaningFSI solves the general FSI problem of 2-D planing surfaces on a free
-surface, where each surface can be rigid or flexible. The substructures are
-part of a rigid body, which can be free in sinkage and trim.
+surface, where each surface can be rigid or flexible.
+Each planing surface must belong to a substructure.
+The substructures are part of a rigid body, which can be free in sinkage and trim.
 
 The fluid solver is based on linearized potential-flow assumptions,
 and the structural solver considers a large-deformation simple beam element.
--- src/planingfsi/__version__.py
+++ src/planingfsi/__version__.py
@@ -1,1 +1,13 @@
-__version__ = "0.1.1"
+"""Load version number from pyproject.toml in project rood directory."""
+import os
+
+import toml
+
+here = os.path.dirname(__file__)
+
+pyproject_path = os.path.join(here, "..", "..", "pyproject.toml")
+
+with open(pyproject_path, "r") as f:
+    dict_ = toml.load(f)
+
+    __version__ = dict_["tool"]["poetry"]["version"]
