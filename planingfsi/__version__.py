010e4ca6c6cd49724aa2762acc82dc9c256de955
Matt Kramer <matthew.robert.kramer@gmail.com>
2018-12-30 21:26:34 -0600

    Fix relative reference

--- planingfsi/__version__.py
+++ planingfsi/__version__.py
@@ -5,7 +5,7 @@ import toml
 
 here = os.path.dirname(__file__)
 
-pyproject_path = os.path.join(here, "..", "..", "pyproject.toml")
+pyproject_path = os.path.join(here, "..", "pyproject.toml")
 
 with open(pyproject_path, "r") as f:
     dict_ = toml.load(f)
