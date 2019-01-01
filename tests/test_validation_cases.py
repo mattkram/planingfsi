f438f7fe46d30adf18261a8eb56aa485516ffc11
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-01-01 14:07:25 -0600

    Add test logic to check for results

--- tests/test_validation_cases.py
+++ tests/test_validation_cases.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 import pytest
 from click.testing import CliRunner
@@ -8,7 +9,7 @@ from planingfsi.cli import main
 
 
 @pytest.fixture()
-def cli_runner(validation_base_dir):
+def cli_runner(tmpdir, validation_base_dir):
     """Return a factory function which can return a case runner in a temporary directory with input files and results
     copied into it.
 
@@ -19,15 +20,15 @@ def cli_runner(validation_base_dir):
 
     def f(case_name):
         runner = CliRunner()
-        with runner.isolated_filesystem():
-            for item in os.listdir(validation_base_dir / case_name):
-                source = validation_base_dir / case_name / item
-                if source.is_dir():
-                    shutil.copytree(source, item)
-                else:
-                    shutil.copy(source, item)
-
-            return runner
+        for item in os.listdir(validation_base_dir / case_name):
+            source = validation_base_dir / case_name / item
+            destination = tmpdir / item
+            if source.is_dir():
+                shutil.copytree(source, destination)
+            else:
+                shutil.copy(source, destination)
+        os.chdir(tmpdir)
+        return runner
 
     return f
 
@@ -36,3 +37,12 @@ def cli_runner(validation_base_dir):
 def test_flat_plate(cli_runner, case_name):
     runner = cli_runner(case_name)
     runner.invoke(main, ["run"])
+
+    validation_extension = ".validated"
+    validation_items = [f for f in os.listdir(".") if f.endswith(validation_extension)]
+
+    # For each validation item, recursively check all files exist and contents are identical
+    for item in validation_items:
+        new_item = item.replace(validation_extension, "")
+        assert Path(new_item).exists()
+        # TODO: Expand tests once results become available
