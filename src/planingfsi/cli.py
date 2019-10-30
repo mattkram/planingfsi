69f05dc63303909b5d463aca70efbc698ac5d407
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-10-29 21:17:48 -0500

    Add type annotations to cli module

--- src/planingfsi/cli.py
+++ src/planingfsi/cli.py
@@ -5,9 +5,9 @@ results from file. It reads the fluid and solid body properties from dictionary
 files, assembles the problem, and runs it.
 
 """
+from pathlib import Path
 
 import click
-import krampy
 
 from . import config
 from . import logger
@@ -19,7 +19,7 @@ from .fsi.simulation import Simulation
 @click.option("post_mode", "--post", is_flag=True)
 @click.option("--plot_save", is_flag=True)
 @click.option("new_case", "--new", is_flag=True)
-def run_planingfsi(post_mode, plot_save, new_case):
+def run_planingfsi(post_mode: bool, plot_save: bool, new_case: bool) -> None:
     if post_mode:
         logger.info("Running in post-processing mode")
         config.plotting.save = True
@@ -29,7 +29,8 @@ def run_planingfsi(post_mode, plot_save, new_case):
         config.plotting.save = True
         config.plotting.plot_any = True
     if new_case:
-        krampy.rm_rf(krampy.find_files(config.path.case_dir, "[0-9]*"))
+        for it_dir in Path(config.path.case_dir).glob("[0-9]*"):
+            it_dir.unlink()
 
     simulation = Simulation()
     simulation.load_input_files()
@@ -41,7 +42,9 @@ def run_planingfsi(post_mode, plot_save, new_case):
 @click.option("plot_show", "--show", is_flag=True)
 @click.option("plot_save", "--save", is_flag=True)
 @click.option("--verbose", is_flag=True)
-def generate_mesh(mesh_dict, plot_show, plot_save, verbose):
+def generate_mesh(
+    mesh_dict: str, plot_show: bool, plot_save: bool, verbose: bool
+) -> None:
     if not mesh_dict:
         config.path.mesh_dict_dir = mesh_dict
 
