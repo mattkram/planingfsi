29b4c7e12b761994fba71d33256b24093d325427
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-11-04 13:40:09 -0600

    Add main CLI command and put run under a subcommand

--- src/planingfsi/cli.py
+++ src/planingfsi/cli.py
@@ -6,6 +6,7 @@ files, assembles the problem, and runs it.
 
 """
 from pathlib import Path
+from typing import Optional
 
 import click
 
@@ -16,6 +17,11 @@ from .fsi.simulation import Simulation
 
 
 @click.group(name="planingfsi", help="Run the PlaningFSI program")
+def cli():
+    pass
+
+
+@cli.command(name="run")
 @click.option("post_mode", "--post", is_flag=True)
 @click.option("--plot_save", is_flag=True)
 @click.option("new_case", "--new", is_flag=True)
@@ -37,20 +43,22 @@ def run_planingfsi(post_mode: bool, plot_save: bool, new_case: bool) -> None:
     simulation.run()
 
 
-@run_planingfsi.command(name="mesh")
+@cli.command(name="mesh")
 @click.argument("mesh_dict", required=False)
 @click.option("plot_show", "--show", is_flag=True)
 @click.option("plot_save", "--save", is_flag=True)
 @click.option("--verbose", is_flag=True)
 def generate_mesh(
-    mesh_dict: str, plot_show: bool, plot_save: bool, verbose: bool
+    mesh_dict: Optional[str], plot_show: bool, plot_save: bool, verbose: bool
 ) -> None:
-    if not mesh_dict:
+    if mesh_dict is not None:
         config.path.mesh_dict_dir = mesh_dict
 
-    mesh = Mesh()
+    if not Path(config.path.mesh_dict_dir).exists():
+        raise FileNotFoundError(f"File {config.path.mesh_dict_dir} does not exist")
 
-    exec(open(config.path.mesh_dict_dir).read())
+    mesh = Mesh()
+    exec(Path(config.path.mesh_dict_dir).open("r").read())
 
     mesh.display(disp=verbose)
     mesh.plot(show=plot_show, save=plot_save)
