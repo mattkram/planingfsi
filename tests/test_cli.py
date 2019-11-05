2080c7c49a881e20c11b60e70346e7a657255a7c
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-11-05 09:55:00 -0600

    Skip ineffective CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,14 +1,16 @@
 import pytest
 
-from planingfsi.cli import run_planingfsi, generate_mesh
+from planingfsi.cli import cli
 
 
-def test_run_planingfsi(runner):
-    results = runner.invoke(run_planingfsi)
+@pytest.mark.skip
+def test_run_planingfsi(runner):    
+    results = runner.invoke(cli, ["run"])
     assert results.exit_code == 0
 
 
+@pytest.mark.skip
 @pytest.mark.skip()
 def test_run_generate_mesh(runner):
-    results = runner.invoke(generate_mesh)
+    results = runner.invoke(cli, ["mesh"])
     assert results.exit_code == 0
