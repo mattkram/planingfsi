27d9bd8f4ef73bb2865a3bfb6f746fe0aa8946e5
Matt Kramer <matthew.robert.kramer@gmail.com>
2019-11-04 22:31:29 -0600

    Clean up trig module and add type hints

--- src/planingfsi/trig.py
+++ src/planingfsi/trig.py
@@ -1,49 +1,14 @@
 """Convenient trigonometric functions."""
 import math
+from typing import Union
 
 import numpy
 
 
-def mag(vec):
-    """Return the magnitude of a vector."""
-    return numpy.linalg.norm(vec)
+ArrayLike = Union[float, numpy.array]
 
 
-def deg2rad(ang):
-    """Convert an angle from degrees to radians.
-
-    Parameters
-    ----------
-    ang : float
-       An angle, in degrees.
-
-    Returns
-    -------
-    float
-       The angle in radians.
-
-    """
-    return ang * numpy.pi / 180
-
-
-def rad2deg(ang):
-    """Convert an angle from radians to degrees.
-
-    Parameters
-    ----------
-    ang : float
-       An angle, in radians.
-
-    Returns
-    -------
-    float
-       The angle in degrees.
-
-    """
-    return ang * 180.0 / numpy.pi
-
-
-def cosd(ang):
+def cosd(ang: ArrayLike) -> ArrayLike:
     """Return the cosine of an angle specified in degrees.
 
     Parameters
@@ -52,10 +17,10 @@ def cosd(ang):
        An angle, in degrees.
 
     """
-    return numpy.cos(deg2rad(ang))
+    return numpy.cos(numpy.radians(ang))
 
 
-def sind(ang):
+def sind(ang: ArrayLike) -> ArrayLike:
     """Return the sine of an angle specified in degrees.
 
     Parameters
@@ -64,10 +29,10 @@ def sind(ang):
        An angle, in degrees.
 
     """
-    return numpy.sin(deg2rad(ang))
+    return numpy.sin(numpy.radians(ang))
 
 
-def tand(ang):
+def tand(ang: ArrayLike) -> ArrayLike:
     """Return the tangent of an angle specified in degrees.
 
     Parameters
@@ -76,10 +41,10 @@ def tand(ang):
        An angle, in degrees.
 
     """
-    return math.tan(deg2rad(ang))
+    return math.tan(numpy.radians(ang))
 
 
-def acosd(slope):
+def acosd(slope: ArrayLike) -> ArrayLike:
     """Return the arccosine of an angle specified in degrees.
 
     Returns
@@ -88,10 +53,10 @@ def acosd(slope):
        An angle, in degrees.
 
     """
-    return rad2deg(math.acos(slope))
+    return numpy.degrees(numpy.arccos(slope))
 
 
-def asind(slope):
+def asind(slope: ArrayLike) -> ArrayLike:
     """Return the arcsine of an angle specified in degrees.
 
     Returns
@@ -100,10 +65,10 @@ def asind(slope):
        An angle, in degrees.
 
     """
-    return rad2deg(math.asin(slope))
+    return numpy.degrees(numpy.arcsin(slope))
 
 
-def atand(slope):
+def atand(slope: ArrayLike) -> ArrayLike:
     """Return the arctangent of an angle specified in degrees.
 
     Returns
@@ -112,10 +77,10 @@ def atand(slope):
        An angle, in degrees.
 
     """
-    return rad2deg(numpy.arctan(slope))
+    return numpy.degrees(numpy.arctan(slope))
 
 
-def atand2(delta_y, delta_x):
+def atand2(delta_y: ArrayLike, delta_x: ArrayLike) -> ArrayLike:
     """Return the arctan2 of an angle specified in degrees.
 
     Returns
@@ -124,69 +89,43 @@ def atand2(delta_y, delta_x):
        An angle, in degrees.
 
     """
-    return rad2deg(numpy.arctan2(delta_y, delta_x))
+    return numpy.degrees(numpy.arctan2(delta_y, delta_x))
 
 
-def ang2vec(ang):
+def ang2vec(ang: ArrayLike) -> ArrayLike:
     """Convert angle in radians to a 3-d unit vector in the x-y plane."""
     return numpy.array([numpy.cos(ang), numpy.sin(ang), 0.0])
 
 
-def ang2vecd(ang):
+def ang2vecd(ang: float) -> numpy.array:
     """Convert angle in degrees to a 3-d unit vector in the x-y plane."""
-    return ang2vec(deg2rad(ang))
+    return ang2vec(numpy.radians(ang))
 
 
-def angd2vec(ang):
+def angd2vec(ang: float) -> numpy.array:
     """Deprecated alias for ang2vecd."""
     return ang2vecd(ang)
 
 
-def angd2vec2d(ang):
+def angd2vec2d(ang: float) -> numpy.array:
     """Convert angle in degrees to a 2-d unit vector in the x-y plane."""
     return ang2vecd(ang)[:2]
 
 
-def quaternion_to_euler_angles(angle, x_comp, y_comp, z_comp):
-    """Convert a quaternion to Euler angles.
-
-    Parameters
-    ----------
-    angle : float
-        The angle to rotate about the component vector.
-    x_comp, y_comp, z_comp : float, float, float
-        The x, y, and z components of the vector about which to rotate.
-
-    """
-    alpha = math.degrees(
-        math.atan2(
-            +2.0 * (angle * x_comp + y_comp * z_comp),
-            +1.0 - 2.0 * (x_comp ** 2 + y_comp ** 2),
-        )
-    )
-
-    beta = math.degrees(
-        math.asin(max([min([+2.0 * (angle * y_comp - z_comp * x_comp), 1.0]), -1.0]))
-    )
-
-    gamma = math.degrees(
-        math.atan2(
-            +2.0 * (angle * z_comp + x_comp * y_comp),
-            +1.0 - 2.0 * (y_comp ** 2 + z_comp ** 2),
-        )
-    )
-
-    return alpha, beta, gamma
-
-
-def rotate_vec_2d(vec, ang):
+def rotate_vec_2d(vec: numpy.array, ang: float) -> numpy.array:
     """Rotate a 2d vector v by angle ang in degrees."""
     vec3d = numpy.zeros(3)
     vec3d[:2] = vec
     return rotate_vec(vec3d, 0, 0, ang)[:2]
 
 
-def rotate_vec(vec, ang_x=0.0, ang_y=0.0, ang_z=0.0, about=numpy.zeros(3)):
+def rotate_vec(
+    vec: numpy.array,
+    ang_x: float = 0.0,
+    ang_y: float = 0.0,
+    ang_z: float = 0.0,
+    about: numpy.array = numpy.zeros(3),
+) -> numpy.array:
     """Rotate a 3d vector v by angle ang in degrees."""
     rot_x = numpy.array(
         [[1, 0, 0], [0, cosd(ang_x), -sind(ang_x)], [0, sind(ang_x), cosd(ang_x)]]
@@ -204,8 +143,3 @@ def rotate_vec(vec, ang_x=0.0, ang_y=0.0, ang_z=0.0, about=numpy.zeros(3)):
         )
         + about
     )
-
-
-def cross2(vec_a, vec_b):
-    """Return the cross product of two 2d vectors."""
-    return vec_a[0] * vec_b[1] - vec_a[1] * vec_b[0]
