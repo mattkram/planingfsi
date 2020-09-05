2f907731fe960f3e0502c6ecc77e123be7aac55b
Matt Kramer <matthew.robert.kramer@gmail.com>
2020-09-05 15:40:19 -0500

    Clean up test module

--- tests/test_validation_cases.py
+++ tests/test_validation_cases.py
@@ -8,45 +8,54 @@ from click.testing import CliRunner
 from planingfsi.cli import cli
 
 
-CliRunnerMaker = Callable[[str], CliRunner]
+VALIDATED_EXTENSION = ".validated"
+
+RunCaseFunction = Callable[[str], Path]
 
 
 @pytest.fixture()
-def cli_runner(tmpdir: Path, validation_base_dir: Path) -> CliRunnerMaker:
-    """Return a factory function which can return a case runner in a temporary directory with input
-    files and results copied into it.
+def run_case(tmpdir: Path, validation_base_dir: Path) -> RunCaseFunction:
+    """A function which can construct a case runner for a specific validation case.
 
-    Returns:
-        function: A function accepting a string which identifies the folder within
-        "validation_cases" to run.
+    The case runner executes in in a temporary directory with input files and results copied into it from the base
+    directory.
 
     """
 
-    def f(case_name: str) -> CliRunner:
+    def f(case_name: str) -> Path:
+        """Create a case runner for a specific case.
+
+        Copies all input files from the base case into a temporary directory in order to compare the contents.
+
+        Args:
+            case_name: The name of the case directory within the validation base directory.
+
+        Returns:
+            The CLI runner object for planingfsi, which can be called via the invoke method.
+
+        """
+
         runner = CliRunner()
-        for source in (validation_base_dir / case_name).glob("*"):
+        case_dir = validation_base_dir / case_name
+        for source in case_dir.glob("*"):
             destination = tmpdir / source.name
-            if source.is_dir():
-                shutil.copytree(str(source), str(destination))
-            else:
-                shutil.copy(str(source), str(destination))
-        os.chdir(str(tmpdir))
-        return runner
+            try:
+                shutil.copytree(source, destination)
+            except NotADirectoryError:
+                shutil.copyfile(source, destination)
+        os.chdir(tmpdir)
+        runner.invoke(cli, ["run"])
+        return Path(tmpdir)
 
     return f
 
 
-@pytest.mark.skip
 @pytest.mark.parametrize("case_name", ("flat_plate",))
-def test_flat_plate(cli_runner: CliRunnerMaker, case_name: str) -> None:
-    runner = cli_runner(case_name)
-    runner.invoke(cli, ["run"])
+def test_run_validation_case(run_case: RunCaseFunction, case_name: str) -> None:
 
-    validation_extension = ".validated"
-    validation_items = [f for f in os.listdir(".") if f.endswith(validation_extension)]
-
+    validation_items = run_case(case_name).glob(f"*{VALIDATED_EXTENSION}")
     # For each validation item, recursively check all files exist and contents are identical
     for item in validation_items:
-        new_item = item.replace(validation_extension, "")
-        assert Path(new_item).exists()
+        new_item = item.with_suffix("")
+        assert new_item.exists()
         # TODO: Expand tests once results become available
