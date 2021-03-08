15ac22d715a2f849b6cff6b4bfae02257b2261e6
Matt Kramer <matthew.robert.kramer@gmail.com>
2021-03-07 22:43:41 -0600

    Add test for flexible membrane simulation

--- tests/test_validation_cases.py
+++ tests/test_validation_cases.py
@@ -71,8 +71,9 @@ def run_case(tmpdir: Path, validation_base_dir: Path) -> RunCaseFunction:
     (
         "flat_plate",
         "stepped_planing_plate",
+        "flexible_membrane",
     ),
-)  # "flexible_membrane")
+)
 def test_run_validation_case(run_case: RunCaseFunction, case_name: str) -> None:
     """For each results directory marked with a '.validated' suffix, check that all newly calculated files exist and
     contents are identical."""
