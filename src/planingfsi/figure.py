d6057227c3b196b491582545314c3f212e431dfc
Matt Kramer <mkramer@anaconda.com>
2022-07-28 15:04:10 -0500

    Add TODO

--- src/planingfsi/figure.py
+++ src/planingfsi/figure.py
@@ -22,6 +22,7 @@ if TYPE_CHECKING:
 
 class FSIFigure:
     def __init__(self, simulation: Simulation, config: Config):
+        # TODO: We can get the config via the simulation in a property
         self.config = config
         self.simulation = simulation
 
