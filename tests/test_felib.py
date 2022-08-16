ce5bec24bc1f41d346b2c91fcd7e1dd7ab981830
Matt Kramer <mkramer@anaconda.com>
2022-08-16 11:26:55 -0500

    Add more basic tests

--- tests/test_felib.py
+++ tests/test_felib.py
@@ -24,3 +24,8 @@ class TestNode:
         """We can set coordinates from any length-two iterable."""
         node = Node(coordinates=coords, node_num=0)
         assert_array_equal(node.coordinates, np.array(coords))
+
+    def test_get_xy_coordinates(self):
+        node = Node(coordinates=np.array([1.0, 2.0]), node_num=0)
+        assert node.x == 1.0
+        assert node.y == 2.0
