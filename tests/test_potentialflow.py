cca9a49d7466ffeeabe786d6fdcbbd68d980d37c
Matt Kramer <mkramer@anaconda.com>
2022-08-23 23:22:59 -0500

    Refactor test assertions

--- tests/test_potentialflow.py
+++ tests/test_potentialflow.py
@@ -18,7 +18,6 @@ def test_grow_points(x0, x1, max_x):
     growth_rate = 1.1
     points = _grow_points(x0, x1, max_x, rate=growth_rate)
     point_ratio = (points[2:] - points[1:-1]) / (points[1:-1] - points[:-2])
-    assert all(p == pytest.approx(growth_rate) for p in point_ratio)
 
     # https://math.stackexchange.com/a/1897065
     n = 0
@@ -27,8 +26,19 @@ def test_grow_points(x0, x1, max_x):
     tmp = (x1 - x0) * growth_rate ** np.array(range(n))
     tmp = np.cumsum(tmp)
     tmp = x0 + np.hstack((np.array([0]), tmp))
-    tmp_ratio = (tmp[2:] - tmp[1:-1]) / (tmp[1:-1] - tmp[:-2])
-    assert all(p == pytest.approx(growth_rate) for p in tmp_ratio)
+
     assert_array_almost_equal(points, tmp)
-    assert x0 in tmp
-    assert x1 in tmp
+
+    # We include the first and second points
+    assert x0 in points
+    assert x1 in points
+
+    # The growth rate is constant and as-specified
+    assert all(p == pytest.approx(growth_rate) for p in point_ratio)
+
+    # Ensure the final point is the only one past max_x
+    next_pt = (points[-1] - points[-2]) * growth_rate + points[-1]
+    if x1 > x0:
+        assert max_x <= points[-1] <= next_pt
+    else:
+        assert max_x >= points[-1] >= next_pt
