2bb905d67c91528b8b9d2229edc7cd26035a0ea3
Matt Kramer <mkramer@anaconda.com>
2022-08-23 23:09:45 -0500

    Parametrize test

--- tests/test_potentialflow.py
+++ tests/test_potentialflow.py
@@ -5,17 +5,24 @@ from numpy.testing import assert_array_almost_equal
 from planingfsi.potentialflow.solver import _grow_points
 
 
-def test_grow_points():
-    x0, x1 = 0.0, -1.0
+@pytest.mark.parametrize(
+    "x0, x1, max_x",
+    [
+        (0.0, 1.0, 20.0),
+        (0.0, -1.0, -20.0),
+        (1.0, 2.0, 51.0),
+        (-1.0, -2.0, -51.0),
+    ],
+)
+def test_grow_points(x0, x1, max_x):
     growth_rate = 1.1
-    max_dist = 20.0
-    points = _grow_points(x0, x1, max_dist, rate=growth_rate)
+    points = _grow_points(x0, x1, max_x, rate=growth_rate)
     point_ratio = (points[2:] - points[1:-1]) / (points[1:-1] - points[:-2])
     assert all(p == pytest.approx(growth_rate) for p in point_ratio)
 
     # https://math.stackexchange.com/a/1897065
     n = 0
-    while (abs(x1 - x0) * (1 - growth_rate**n) / (1 - growth_rate)) < max_dist:
+    while (abs(x1 - x0) * (1 - growth_rate**n) / (1 - growth_rate)) < abs(max_x):
         n += 1
     tmp = (x1 - x0) * growth_rate ** np.array(range(n))
     tmp = np.cumsum(tmp)
